e550e53ac7bf08444c016f6eb0ba63e2867c26e1
GOliveira15 <gabrielosilva15@gmail.com>
2021-12-30 16:38:29 -0300

    Aulas

---
+++ aula_javascript/atividade/script.js
@@ -0,0 +1,17 @@
+function operacao(n1,n2){
+    if(n1 == n2){
+
+    }
+    else{
+
+    }
+
+    let soma = n1 + n2;
+
+    if(soma )
+}
+
+var n1 = parseFloat(prompt('Insira um número: '));
+var n2 = parseFloat(prompt('Insira outro número: '));
+
+operacao(n1,n2)
--- aula_javascript/to-do_list/assets/js/script.js
+++ aula_javascript/to-do_list/assets/js/script.js
@@ -2,9 +2,14 @@ var list = [];
 var atividade;
 
 function adicionar(){
-    atividade = document.getElementById('tarefa');
+    atividade = document.getElementById('tarefa').value;
     list.push(atividade);
-
-    
 }
 
+function exibir(lista){
+    for (let i = 0; i < list.length; i++){
+        document.getElementById('exibir').innerHTML += `<input type="checkbox" id="atividade" name="atividade"><label for="atividade">${list[i]}</label><br>`;
+    }
+}
+
+document.getElementById('exibir').innerHTML(exibir(list));
